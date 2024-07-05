90f5aadc0eee0ddad87912fd2870b36bd2b31f7f
yijun xu <yijun@ucare.io>
2024-07-05 08:54:15 +0800

    add grpc with gevent

--- app/patched.py
+++ app/patched.py
@@ -7,5 +7,22 @@ def get_patched_app():
 
     return app
 
+# to enable gRPC!
+# def get_patched_app():
+
+
+#     from gevent import monkey
+#     monkey.patch_all()
+
+#     import grpc._cython.cygrpc
+#     grpc._cython.cygrpc.init_grpc_gevent()
+
+#     from app.websocket_app import app
+
+#     return app
+
+
+# app = get_patched_app()
+
 
 app = get_patched_app()
